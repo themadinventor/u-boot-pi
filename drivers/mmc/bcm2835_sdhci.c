1c2b43a634955661d6f7b69459de00ffd1c2d7a6
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2012-07-03 19:57:37 -0700

    Workaround insufficient timeouts in u-boot

--- drivers/mmc/bcm2835_sdhci.c
+++ drivers/mmc/bcm2835_sdhci.c
@@ -28,15 +28,55 @@
 #include <asm/arch/regs.h>
 
 #undef BCM2835_TRACE_REGISTERS
-#define WRITE_DELAY	40
+
+#define	MAX_FREQ	80000000
+#define	MIN_FREQ	((MAX_FREQ/1023)/2)
 
 static char *BCMSDH_NAME = "bcm2835_sdh";
 static struct sdhci_ops bcm2835_ops;
+uint twoticks_delay = 0;
+unsigned long long last_write = 0;
 
 inline static void bcm2835_sdhci_raw_writel(struct sdhci_host *host, u32 val, int reg)
 {
+
+	/* 
+	 *The Arasan has a bugette whereby it may lose the content of
+	 * successive writes to registers that are within two SD-card clock
+   	 * cycles of each other (a clock domain crossing problem).
+	 * It seems, however, that the data register does not have this problem.
+	 * (Which is just as well - otherwise we'd have to nobble the DMA engine
+	 * too)
+	 */
+	while (get_timer(last_write) < twoticks_delay) {
+
+	}
+
 	writel(val, host->ioaddr + reg);
-	udelay(20000);
+	last_write = get_timer(0);
+
+	/*
+	 * Hackish. It seems that some U-Boot timeouts are not 
+	 * long enough for the SD card working on default frequency
+	 * so until we switch to full speed - create artificial delays
+	 */
+	if (host->mmc->clock <= MIN_FREQ) {
+		udelay(50000);
+	}
+
+	if ((reg != SDHCI_BUFFER && reg != SDHCI_INT_STATUS && reg != SDHCI_CLOCK_CONTROL)
+	    && host->mmc->clock > MIN_FREQ)
+  	{
+		int timeout = 100000;
+		while (val != readl(host->ioaddr + reg) && --timeout > 0)
+		   continue;
+
+		if (timeout <= 0)
+			printf("bcm2835_sdhci: writing 0x%X to reg 0x%X "
+				   "always gives 0x%X\n",
+				   val, reg, readl(host->ioaddr + reg));
+	}
+
 }
 
 inline static u32 bcm2835_sdhci_raw_readl(struct sdhci_host *host, int reg)
@@ -124,6 +164,7 @@ static u8 bcm2835_sdhci_readb(struct sdhci_host *host, int reg)
 int bcm2835_sdh_init(u32 regbase)
 {
 	struct sdhci_host *host = NULL;
+	uint32_t clock;
 
 	host = (struct sdhci_host *)malloc(sizeof(struct sdhci_host));
 	if (!host) {
@@ -133,7 +174,7 @@ int bcm2835_sdh_init(u32 regbase)
 
 	host->name = BCMSDH_NAME;
 	host->ioaddr = (void *)regbase;
-	host->quirks = SDHCI_QUIRK_BROKEN_VOLTAGE;
+	host->quirks = SDHCI_QUIRK_BROKEN_VOLTAGE | SDHCI_QUIRK_BROKEN_R1B;
 	host->voltages = MMC_VDD_32_33 | MMC_VDD_33_34 | MMC_VDD_165_195;;
 
 	memset(&bcm2835_ops, 0, sizeof(struct sdhci_ops));
@@ -145,8 +186,14 @@ int bcm2835_sdh_init(u32 regbase)
 	bcm2835_ops.read_b = bcm2835_sdhci_readb;
 	host->ops = &bcm2835_ops;
 
+	/*
+	 * Convert SD controller ticks to CPU ticks
+	 */
+	twoticks_delay = ((2000000000ULL/MIN_FREQ) + 1)*get_tbclk()/1000000;
+	twoticks_delay = (twoticks_delay + 1000 - 1) / 1000;
+
 	host->version = sdhci_readw(host, SDHCI_HOST_VERSION) & 0xff;
-	add_sdhci(host, 80000000, 0);
+	add_sdhci(host, MIN_FREQ, 0);
 
 	return 0;
 }
